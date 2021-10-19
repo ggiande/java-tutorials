228c4e53ea717a25e8365c381b1907b925cdd190
Giancarlo <ggarciadeleon@csustan.edu>
2021-10-18 21:58:41 -0700

    added functions and javadocs for Array Class

--- src/main/java/ggiande/tutorial/datastructure/Array.java
+++ src/main/java/ggiande/tutorial/datastructure/Array.java
@@ -1,13 +1,11 @@
 package ggiande.tutorial.datastructure;
 
 /**
-* Demonstrate the abilities of
-* an Array as well as it's limitations
+* Demonstrate the abilities of an array as well as it's limitations.
 */
 public class Array {
 
-    /** Prints all values from an array by
-     * iterating through their indexes
+    /** ACCESS ALL values from an array by iterating through their indexes.
      * @param arrayOfIntegers An array of integers
      */
     public void printArrayElementsByIndex(int[] arrayOfIntegers) {
@@ -16,8 +14,8 @@ public class Array {
         }
     }
 
-    /** Prints all elements from an array by iterating through the array's elements.
-     * @param arrayOfIntegers An array of integers */
+    /** ACCESS ALL elements from an array by iterating through the array's elements.
+     * @param arrayOfIntegers an array of integers */
     public void printArrayElements(int[] arrayOfIntegers) {
         for (Integer number : arrayOfIntegers) {
             System.out.println(number);
@@ -25,9 +23,10 @@ public class Array {
     }
 
     /**
-     * Removes an element from an array.
+     * REMOVES an element from an array.
      * @param index index to remove from an array
-     *
+     * @param arrayOfIntegers an array of integers
+     * Throws IndexOutOfBoundsException when index is not in array
      */
     public void removeElementFromArray(int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
         for (int i = index; i < arrayOfIntegers.length - 1; i++) {
@@ -35,4 +34,32 @@ public class Array {
         }
     }
 
+    /**
+     * ACCESS A specific value from an array given an index.
+     * @param index index to access from an array
+     * @param arrayOfIntegers an array of integers
+     * Throws IndexOutOfBoundsException when index is not in array
+     * */
+    public void accessValue (int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
+        int valueFromIndex = 0;
+        valueFromIndex = arrayOfIntegers[index];
+        System.out.println(valueFromIndex);
+    }
+
+    /**
+     * UPDATE a value from an array given an index.
+     * @param arrayOfIntegers an array of integers
+     * @param index index to access from an array
+     * @param value value to replace the value at the index
+     * Throws IndexOutOfBoundsException when index is not in array */
+    public void updateValue(int[] arrayOfIntegers, int index, int value) throws IndexOutOfBoundsException {
+        arrayOfIntegers[index] = value;
+    }
+    /**
+     * RETRIEVES the length of the array.
+     * @param arrayOfIntegers an array of integers
+     * Throws IndexOutOfBoundsException when index is not in array */
+    public void getArrayLength(int[] arrayOfIntegers) {
+        System.out.println("Size of Array: " + arrayOfIntegers.length);
+    }
 }
