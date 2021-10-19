769a1c5740eb2ca317adb0d2abf7b4a4736a380c
ggiande <ggarciadeleon@csustan.edu>
2021-10-18 21:48:57 -0700

    Crateed Array Test Class

---
+++ src/test/java/ggiande/tutorial/datastructure/ArrayTest.java
@@ -0,0 +1,28 @@
+package ggiande.tutorial.datastructure;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTest {
+  int[] arrayOfIntegers;
+  @BeforeEach
+  void setUp() {
+    // TODO: Init the Array that will always be used
+    arrayOfIntegers = new int[]{1, 2, 3, 4, 5};
+  }
+
+  @Test
+  void printArrayElementsByIndex(int[] arrayOfIntegers) {
+
+  }
+
+  @Test
+  void printArrayElements() {
+  }
+
+  @Test
+  void removeElementFromArray() {
+  }
+}
