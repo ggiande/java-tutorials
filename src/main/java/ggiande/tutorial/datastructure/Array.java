85c6367c9776b144bae03bcc7fa6b61d03869e9d
ggiande <ggarciadeleon@csustan.edu>
2021-10-20 13:51:51 -0700

    increased test coverage

--- src/main/java/ggiande/tutorial/datastructure/Array.java
+++ src/main/java/ggiande/tutorial/datastructure/Array.java
@@ -1,65 +1,92 @@
 package ggiande.tutorial.datastructure;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Arrays;
+
 /**
-* Demonstrate the abilities of an array as well as it's limitations.
+* Demonstrate the abilities of an array as well as its limitations.
 */
 public class Array {
 
-    /** ACCESS ALL values from an array by iterating through their indexes.
-     * @param arrayOfIntegers An array of integers
-     */
-    public void printArrayElementsByIndex(int[] arrayOfIntegers) {
+    public Array() { }
+
+    private static final Logger log = LogManager.getLogger(Array.class);
+
+    private static void printArrayElementsByIndex(int[] arrayOfIntegers) {
         for (int i = 0; i < arrayOfIntegers.length; i++) {
-            System.out.println(arrayOfIntegers[i]);
+            int currentValue = arrayOfIntegers[i];
+            log.info("Array Index: {}", i + " Value: " + currentValue);
         }
     }
 
     /** ACCESS ALL elements from an array by iterating through the array's elements.
-     * @param arrayOfIntegers an array of integers */
-    public void printArrayElements(int[] arrayOfIntegers) {
-        for (Integer number : arrayOfIntegers) {
-            System.out.println(number);
-        }
+     *
+     * @param arrayOfIntegers an array of integers
+     * @return Returns a string of the printed array of integers */
+    public String printArrayElements(int[] arrayOfIntegers) {
+        return String.join(", ", Arrays.toString(arrayOfIntegers));
     }
 
+
     /**
-     * REMOVES an element from an array.
+     * REMOVES an element by finding the element at the specified index
+     * and then removing that element. The rest of the elements are copied
+     * into a new array. This would lead to an array of size one less
+     * than the original array.
+     *
      * @param index index to remove from an array
      * @param arrayOfIntegers an array of integers
+     * @return Returns an array without the removed element
      * Throws IndexOutOfBoundsException when index is not in array
      */
-    public void removeElementFromArray(int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
-        for (int i = index; i < arrayOfIntegers.length - 1; i++) {
-            arrayOfIntegers[i] = arrayOfIntegers[i + 1];
+    public int[] removeElementFromArray(int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
+        if (arrayOfIntegers == null || index < 0 || index >= arrayOfIntegers.length) {
+            return arrayOfIntegers;
         }
+
+        int[] anotherArray = new int[arrayOfIntegers.length - 1];
+        for (int i = 0, k = 0; i < arrayOfIntegers.length; i++) {
+            if (i == index) {
+                continue;
+            }
+            anotherArray[k++] = arrayOfIntegers[i];
+        }
+        log.info("New Array: " + Arrays.toString(anotherArray));
+        return anotherArray;
     }
 
     /**
      * ACCESS A specific value from an array given an index.
+     *
      * @param index index to access from an array
      * @param arrayOfIntegers an array of integers
+     * @return Returns the value of the index accessed in the array
      * Throws IndexOutOfBoundsException when index is not in array
      * */
-    public void accessValue (int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
-        int valueFromIndex = 0;
-        valueFromIndex = arrayOfIntegers[index];
-        System.out.println(valueFromIndex);
+    public int accessValue (int[] arrayOfIntegers, int index) throws IndexOutOfBoundsException {
+        int valueFromIndex = arrayOfIntegers[index];
+        return valueFromIndex;
     }
 
     /**
      * UPDATE a value from an array given an index.
+     *
      * @param arrayOfIntegers an array of integers
      * @param index index to access from an array
      * @param value value to replace the value at the index
      * Throws IndexOutOfBoundsException when index is not in array */
-    public void updateValue(int[] arrayOfIntegers, int index, int value) throws IndexOutOfBoundsException {
+    public int[] updateValue(int[] arrayOfIntegers, int index, int value) throws IndexOutOfBoundsException {
         arrayOfIntegers[index] = value;
+        return arrayOfIntegers;
     }
+
     /**
      * RETRIEVES the length of the array.
+     *
      * @param arrayOfIntegers an array of integers
      * Throws IndexOutOfBoundsException when index is not in array */
-    public void getArrayLength(int[] arrayOfIntegers) {
-        System.out.println("Size of Array: " + arrayOfIntegers.length);
+    public int getArrayLength(int[] arrayOfIntegers) {
+        return arrayOfIntegers.length;
     }
 }
--- src/test/java/ggiande/tutorial/datastructure/ArrayTest.java
+++ src/test/java/ggiande/tutorial/datastructure/ArrayTest.java
@@ -2,58 +2,53 @@ package ggiande.tutorial.datastructure;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
-import java.util.ArrayList;
-
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArrayTest {
-  int[] arrayOfIntegers;
 
-  @Mock
-  Array arrayClass;
+  int[] arrayOfIntegers;
+  Array array;
+
   @BeforeEach
   void setUp() {
-    // TODO: Init the Array that will always be used
     arrayOfIntegers = new int[]{1, 2, 3, 4, 5};
-  }
-
-  @Test
-  void printArrayElementsByIndex(int[] arrayOfIntegers) {
-//    verify(arrayClass.printArrayElementsByIndex(arrayOfIntegers), isA(ArrayList()));
-  }
-
-  @Test
-  void printArrayElements() {
-  }
-
-  @Test
-  void removeElementFromArray() {
-  }
-
-  @Test
-  void testPrintArrayElementsByIndex() {
+    array = new Array();
   }
 
   @Test
   void testPrintArrayElements() {
+    String expectedString = "[1, 2, 3, 4, 5]";
+    Array array = new Array();
+    assertEquals(expectedString, array.printArrayElements(arrayOfIntegers));
   }
 
   @Test
   void testRemoveElementFromArray() {
+    int index = 2;
+    int[] updatedArrayOfIntegers = new int[]{1, 2, 4, 5};
+    assertArrayEquals(updatedArrayOfIntegers, array.removeElementFromArray(arrayOfIntegers, index));
   }
 
   @Test
   void accessValue() {
+    int index = 3;
+    int accessedValue = arrayOfIntegers[3];
+    assertEquals(accessedValue, array.accessValue(arrayOfIntegers, index));
   }
 
   @Test
   void updateValue() {
+    int index = 3;
+    int value = 100;
+    arrayOfIntegers[index] = value;
+    assertArrayEquals(arrayOfIntegers, array.updateValue(arrayOfIntegers, index, value));
   }
 
   @Test
   void getArrayLength() {
+    int lengthOfArray = arrayOfIntegers.length;
+    assertEquals(lengthOfArray, array.getArrayLength(arrayOfIntegers));
   }
 }
